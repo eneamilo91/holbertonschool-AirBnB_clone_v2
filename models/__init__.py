25b317a89b9ebc09366627d9b4ad3cf4213c1243
eneamilo91 <eneamilo514@gmail.com>
2024-03-27 00:06:33 +0000

    updated package definator with new methods

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
-from models.engine.file_storage import FileStorage
-from models.engine.db_storage import DBStorage
+
+
 from os import getenv
 from models.amenity import Amenity
 from models.state import State
@@ -12,8 +12,10 @@ from models.user import User
 from models.place import Place
 
 if getenv("HBNB_TYPE_STORAGE") == 'db':
+    from models.engine.db_storage import DBStorage
     storage = DBStorage()
     storage.reload()
 else:
+    from models.engine.file_storage import FileStorage
     storage = FileStorage()
     storage.reload()
