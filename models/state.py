dac1674faca37341ba0eb5d581822525f049ea5b
eneamilo91 <eneamilo514@gmail.com>
2024-03-27 00:02:30 +0000

    updated state with new methods

--- models/state.py
+++ models/state.py
@@ -1,26 +1,28 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from models.state import Base
+from sqlalchemy import Column, Integer,\
+    String, DateTime, ForeignKey
 from sqlalchemy.orm import relationship
 from os import getenv
-from models.city import City
-from models import storage
 
 
 class State(BaseModel, Base):
     """ State class """
+
     __tablename__ = "states"
-    name = Column(String(128), nullable=False)
+
+    name = Column(String(128),
+                  nullable=False)
 
     if getenv("HBNB_TYPE_STORAGE") == 'db':
-        cities = relationship("City", back_populates="state", cascade="delete")
+        cities = relationship("City", backref="state", cascade="delete")
     else:
         @property
         def cities(self):
-            list_of_cities = []
+            from models.city import City
+            from models import storage
 
-            for key, value in storage.all(City).items():
-                if value.to_dict()['state_id'] == self.id:
-                    list_of_cities.append(value)
-            return list_of_cities
+            return [value for key, value in storage.all(City).items()
+                    if value.to_dict()['state_id'] == self.id]
