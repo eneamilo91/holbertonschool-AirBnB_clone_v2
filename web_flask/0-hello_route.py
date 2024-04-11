224226859965995d1bce628a457fa7b8ad0466bd
eneamilo91 <eneamilo514@gmail.com>
2024-04-11 20:56:41 +0000

    added file

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,26 +1,19 @@
-<<<<<<< HEAD
-=======
 #!/usr/bin/python3
-"""Module of a python script"""
+"""
+Module to initiate a flask app
+"""
 
->>>>>>> f8fc929b8545ce466a6173b756146a580a15b191
 from flask import Flask
-
 app = Flask(__name__)
 
-<<<<<<< HEAD
+
 @app.route('/', strict_slashes=False)
 def index():
-    return "Hello HBNB!"
+    """
+    Index route
+    """
+    return 'Hello HBNB!'
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
-=======
-@app.route('/',strict_slashes=False)
-def home():
-    """func to be executed"""
-    return "Hello HBNB!"
 
-if __name__ =='__main__':
-    app.run(host="0.0.0.0", port=5000)
->>>>>>> f8fc929b8545ce466a6173b756146a580a15b191
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
